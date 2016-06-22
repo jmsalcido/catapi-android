a223f6bb5d4751120f217b2c8030014bcccc49ac
jmsalcido <eljose152@gmail.com>
2016-06-21 19:15:27 -0700

    remove unused code

--- app/src/main/java/org/otfusion/caturday/util/ApplicationUtils.java
+++ app/src/main/java/org/otfusion/caturday/util/ApplicationUtils.java
@@ -46,32 +46,6 @@ public class ApplicationUtils {
         return shareIntent;
     }
 
-    public static Uri getLocalBitmapUri(ImageView imageView) {
-        // Extract Bitmap from ImageView drawable
-        Drawable drawable = imageView.getDrawable();
-        Bitmap bmp;
-        if (drawable instanceof BitmapDrawable){
-            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-        } else {
-            return null;
-        }
-        // Store image to default external storage directory
-        Uri bmpUri = null;
-        try {
-            File file =  new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
-            file.getParentFile().mkdirs();
-            FileOutputStream out = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
-            out.close();
-            bmpUri = Uri.fromFile(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bmpUri;
-    }
-
-
     public static String generateRandomCatName(String suffix) {
         List<String> differentNames = Arrays.asList(
                 "aegean",
