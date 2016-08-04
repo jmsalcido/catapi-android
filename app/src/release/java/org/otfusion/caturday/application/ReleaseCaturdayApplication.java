f159c97def413c78bed83507f3378e9194702e38
jmsalcido <eljose152@gmail.com>
2016-08-03 22:55:27 -0700

    fix travis build: - use buildToolsVersion 23.0.1 on travis and 24+ (latest) on local
    Crashlytics:
    - add git_sha and buildtime

--- app/src/release/java/org/otfusion/caturday/application/ReleaseCaturdayApplication.java
+++ app/src/release/java/org/otfusion/caturday/application/ReleaseCaturdayApplication.java
@@ -2,6 +2,8 @@ package org.otfusion.caturday.application;
 
 import com.crashlytics.android.Crashlytics;
 
+import org.otfusion.caturday.BuildConfig;
+
 import io.fabric.sdk.android.Fabric;
 
 public class ReleaseCaturdayApplication extends CaturdayApplication {
@@ -9,6 +11,12 @@ public class ReleaseCaturdayApplication extends CaturdayApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        setupCrashlytics();
+    }
+
+    private void setupCrashlytics() {
         Fabric.with(this, new Crashlytics());
+        Crashlytics.setString("GIT_SHA_KEY", BuildConfig.GIT_SHA);
+        Crashlytics.setString("BUILD_TIME_KEY", BuildConfig.BUILD_TIME);
     }
 }
